defcb0ebc18e39cc23a83b4e3d1fc02b2e0d1c8c
Brian Oduor <brianoduor03@gmail.com>
2025-05-14 23:18:52 +0300

    host change

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   server: {
     host: true,
     port: parseInt(process.env.PORT) || 5173,
-    allowedHosts: 'all',
+    allowedHosts: 'student-voting-frontend-2.onrender.com',
   },
 });
